1253eb69283df22f6a8427d0a7e22308ee77c14f
Mani S Tiwari <tiwari.mani@tftus.com>
2025-12-16 11:00:30 +0530

    Delete uninst.ps1

--- uninst.ps1
+++
@@ -1,7 +0,0 @@
-$path = (Get-Command selfbest).Source
-if ($path) {
-    Remove-Item $path -Force
-    Write-Host "Deleted selfbest binary at: $path"
-} else {
-    Write-Host "selfbest executable not found in PATH."
-}
